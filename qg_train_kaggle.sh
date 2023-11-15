216993d1c0125d9a41ca883b1ea7220a0e2f146d
PhuongN <thuphuong.nguyen.cntt@gmail.com>
2023-11-15 10:17:28 +0700

    [b] Reduce batch size to fit GPU memory

--- qg_train_kaggle.sh
+++ qg_train_kaggle.sh
@@ -1,6 +1,7 @@
 python3 QG_train.py \
     --n_epochs 4 \
     --local_rank 0 \
+    --train_batch_size 2 \
     --model_name_or_path vinai/bartpho-syllable \
     --output_dir /kaggle/working/vn-qa-gen/output/bartpho-syllable \
     --train_dataset_path /kaggle/working/vn-qa-gen/Datasets/ViQuAD1.0/train_ViQuAD.json \
