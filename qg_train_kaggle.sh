2e93ec2295c41117edb5e35598ae4ab871a65290
PhuongN <thuphuong.nguyen.cntt@gmail.com>
2023-11-15 10:08:56 +0700

    [c] Allow using gpu for training with kaggle

--- qg_train_kaggle.sh
+++ qg_train_kaggle.sh
@@ -1,6 +1,6 @@
 python3 QG_train.py \
-    --eval_before_start \
     --n_epochs 4 \
+    --local_rank 0 \
     --model_name_or_path vinai/bartpho-syllable \
     --output_dir /kaggle/working/vn-qa-gen/output/bartpho-syllable \
     --train_dataset_path /kaggle/working/vn-qa-gen/Datasets/ViQuAD1.0/train_ViQuAD.json \
