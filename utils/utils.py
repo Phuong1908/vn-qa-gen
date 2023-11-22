00b7732e31bbbbc8d0caf34bd36b8b0e31619748
PhuongN <thuphuong.nguyen.cntt@gmail.com>
2023-11-22 19:10:50 +0700

    [f] Implement logic to upload trained weight to drive

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,61 @@
 import os
 import sys
 import pickle
+import json
+import shutil
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
+
+DRIVE_OUTPUT_PATH = '/Master_Thesis/Vietnamese/output/'
+
+def gg_auth():
+  gauth = GoogleAuth()
+  # Try to load saved client credentials
+  gauth.LoadCredentialsFile("mycreds.txt")
+  if gauth.credentials is None:
+    # Authenticate if they're not there
+    gauth.LocalWebserverAuth()
+  elif gauth.access_token_expired:
+    # Refresh them if expired
+    gauth.Refresh()
+  else:
+    # Initialize the saved creds
+    gauth.Authorize()
+  # Save the current credentials to a file
+  gauth.SaveCredentialsFile("mycreds.txt")
+  return gauth
+  
+
+def save_result_to_drive(epoch, path, prefix, score, save_checkpoint):
+  filename = f'{prefix}.epoch{epoch}'
+  result_folder = f'{path}/{filename}'
+  os.mkdir(result_folder)
+  
+  # save score into json file
+  with open(f'{result_folder}/training_result.json', "w") as outfile: 
+    json.dump(score, outfile)
+    
+  # save model into file
+  if callable(save_checkpoint):
+    save_checkpoint()
+  else:
+    print("ERROR!!!: save_checkpoint must be callable")
+    
+  # compress folder
+  shutil.make_archive(result_folder, 'zip', result_folder)
+
+  #upload into drive
+  gauth = gg_auth()
+  drive = GoogleDrive(gauth)
+  
+  file_path = f'{path}/{filename}.zip'
+  drive_file_path = f'{DRIVE_OUTPUT_PATH}/{filename}.zip'
+
+  file_drive = drive.CreateFile({"title": drive_file_path}) 
+  file_drive.SetContentFile(file_path)
+  file_drive.Upload()
+  print(f"Uploaded file with ID {file_drive.get('id')}")
+
 
 def normalize_text(text):
     """
@@ -51,3 +106,15 @@ def load(filepath, message=None):
     print("Saving {}...".format(message))
   result = pickle_load_large_file(filepath)
   return result
+
+if __name__ == "__main__":
+  path = '/Users/phuongnguyen/study/vn-qa-gen/output/bartpho-syllable'
+  score = {
+    "nll": 111,
+    "ppl": 111,
+  }
+  def save_checkpoint():
+    print('checkpoint saved!')
+    return
+  
+  save_result_to_drive(epoch=1, path=path, prefix='epx', score=score, save_checkpoint=save_checkpoint)
