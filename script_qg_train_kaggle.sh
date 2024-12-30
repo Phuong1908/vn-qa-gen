a8832bf8b3a07db787ec3b770d3e07378e3f91fd
PhuongN <thuphuong.nguyen.cntt@gmail.com>
2024-12-30 22:35:48 +0700

    [exp] Start training from scratch

--- script_qg_train_kaggle.sh
+++ script_qg_train_kaggle.sh
@@ -3,10 +3,11 @@ python3 QG_train.py \
     --eval_before_start \
     --local_rank 0 \
     --train_batch_size 2 \
-    --model_name_or_path /kaggle/working/vn-qa-gen/model_param  \
+    --model_name_or_path vinai/bartpho-syllable  \
     --output_dir /kaggle/working/vn-qa-gen/output/bartpho-syllable \
     --train_dataset_path /kaggle/working/vn-qa-gen/datasets/ViQuAD1.0/train_ViQuAD.json \
     --dev_dataset_path /kaggle/working/vn-qa-gen/datasets//ViQuAD1.0/dev_ViQuAD.json \
     --train_dataset_cache_path /kaggle/working/vn-qa-gen/datasets/QG/train_cache.pkl \
     --dev_dataset_cache_path /kaggle/working/vn-qa-gen/datasets/QG/dev_cache.pkl
 
+#     --model_name_or_path /kaggle/working/vn-qa-gen/model_param  \
