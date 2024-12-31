d9ea02b9ec0a8d3c65d16c48d28c6995d098f9e3
PhuongN <thuphuong.nguyen.cntt@gmail.com>
2024-12-31 11:35:08 +0700

    [exp] Attempt to increase batch size

--- script_qg_train_kaggle.sh
+++ script_qg_train_kaggle.sh
@@ -2,7 +2,7 @@ python3 QG_train.py \
     --n_epochs 4 \
     --eval_before_start \
     --local_rank 0 \
-    --train_batch_size 2 \
+    --train_batch_size 4 \
     --model_name_or_path vinai/bartpho-syllable  \
     --output_dir /kaggle/working/vn-qa-gen/output/bartpho-syllable \
     --train_dataset_path /kaggle/working/vn-qa-gen/datasets/ViQuAD1.0/train_ViQuAD.json \
